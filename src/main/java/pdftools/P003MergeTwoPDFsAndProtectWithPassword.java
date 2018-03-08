bcd41363fb71c3d669673214cc0f23943ffd5787
Igor Repinetski <zefer@gmx.de>
2018-03-08 02:13:29 +0100

    More examples added

--- src/main/java/pdftools/P003MergeTwoPDFsAndProtectWithPassword.java
+++ src/main/java/pdftools/P003MergeTwoPDFsAndProtectWithPassword.java
@@ -5,18 +5,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
+import com.pd4ml.Constants;
 import com.pd4ml.PD4ML;
 import com.pd4ml.PdfDocument;
 
 /**
  * Generates PDF from a simple HTML string. Page format, margins etc are default.
- * Merges two PDF files. In the example it merges with itself. 
+ * Merges two PDF files. In the example it merges with itself and protects with password. 
  */
-public class P002MergeTwoPDFs {
+public class P003MergeTwoPDFsAndProtectWithPassword {
     public static void main( String[] args ) throws IOException {
-
-    	// The simplest way, but not convenient for current "java:"-protocol-addressed resources:
-    	// PdfDocument.mergePDFs(new FileInputStream(args[0]), new FileInputStream(args[1]), new FileOutputStream(args[2]));  
 
     	new PD4ML(); // we instantiate it here only to register "java:" protocol
     	
@@ -27,7 +25,8 @@ public class P002MergeTwoPDFs {
 		File f = File.createTempFile("pdf", ".pdf");
 		
 		pdf1.append(pdf2);
-		pdf1.write(new FileOutputStream(f));
+		pdf1.write(new FileOutputStream(f), "test",   // Protect the resulting PDF with password "test"
+                Constants.AllowDegradedPrint | Constants.AllowAnnotate);
 
     	// open the just-generated PDF with a default PDF viewer
     	Desktop.getDesktop().open(f);
--- src/main/java/pdftools/P004UpdatePdfMetaInfo.java
+++ src/main/java/pdftools/P004UpdatePdfMetaInfo.java
@@ -10,24 +10,26 @@ import com.pd4ml.PdfDocument;
 
 /**
  * Generates PDF from a simple HTML string. Page format, margins etc are default.
- * Merges two PDF files. In the example it merges with itself. 
+ * Merges two PDF files. In the example it merges with itself and protects with password. 
  */
-public class P002MergeTwoPDFs {
+public class P004UpdatePdfMetaInfo {
     public static void main( String[] args ) throws IOException {
-
-    	// The simplest way, but not convenient for current "java:"-protocol-addressed resources:
-    	// PdfDocument.mergePDFs(new FileInputStream(args[0]), new FileInputStream(args[1]), new FileOutputStream(args[2]));  
 
     	new PD4ML(); // we instantiate it here only to register "java:" protocol
     	
 		URL pdfUrl = new URL("java:/pdftools/PDFOpenParameters.pdf");
-		PdfDocument pdf1 = new PdfDocument(pdfUrl, null);
-		PdfDocument pdf2 = new PdfDocument(pdfUrl, null);
+		PdfDocument doc = new PdfDocument(pdfUrl, null);
 		
+        System.out.println("document author: " + doc.getAuthor());  
+          
+        doc.setTitle("Document Modification Test");  
+        doc.setSubject("PdfDocument API test");  
+        doc.setKeywords("key1, key2");  
+        doc.setModDate(); // set modification date to NOW  
+  
 		File f = File.createTempFile("pdf", ".pdf");
-		
-		pdf1.append(pdf2);
-		pdf1.write(new FileOutputStream(f));
+
+		doc.write(new FileOutputStream(f), null, -1); // no password, default permissions  
 
     	// open the just-generated PDF with a default PDF viewer
     	Desktop.getDesktop().open(f);
